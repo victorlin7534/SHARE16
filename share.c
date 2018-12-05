60bf23c3f2c192ecb2e2d6619ba42dc64dc83e9f
victorlin7534 <vlin5@stuy.edu>
2018-12-04 20:38:56 -0500

    1

---
+++ share.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+void receive_input(char * statement, char * input, int size){
+  printf("%s\n",statement);
+  fgets(input,size,stdin);
+  strtok(input,"\n");
+}
+
+int main(){
+  key_t key = ftok("share.c", 999);
+  int shmid = shmget(key, 200, 0644 | IPC_CREAT);
+  char * data = shmat(shmid,0,0);
+  char in[100]; 
+  while(1){
+    printf("segment contains: %s\n", data);
+    receive_input("Do you want to change data in segment? (type y/n)",in,100);
+    if(!strcmp(in,"y")){
+      receive_input("Delete or modify? (type d/a)",in,100);
+      if(!strcmp(in,"d")){
+        shmdt(data);
+        shmctl(shmid, IPC_RMID, NULL);
+        break;
+      } 
+      else if(!strcmp(in,"a")) receive_input("type your modification: ",data,200);
+    }
+  }
+  return 0;
+}
